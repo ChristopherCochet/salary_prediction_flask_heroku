ebf68ebe3f01b8fcb381f15ff618f9f5b59d8729
christophercochet <christopher.m.cochet@gmail.com>
2020-05-17 18:14:20 +1200

    fix to flask app

--- salary_prediction/salary_prediction_app.py
+++ salary_prediction/salary_prediction_app.py
@@ -1,8 +1,1 @@
-from flask import Flask
-
-salary_prediction_app = Flask(__name__)
-
-@salary_prediction_app.route('/')
-@salary_prediction_app.route('/index')
-def index():
-    return "<H1>Hello, World, this is Chris!</H1>"
+from salary_prediction import salary_prediction_app
