fa4e9e267dba7a13f6ce3897849c3950a2589070
christophercochet <christopher.m.cochet@gmail.com>
2020-05-17 18:03:01 +1200

    udpate to python flask code

--- salary_prediction/salary_prediction_app.py
+++ salary_prediction/salary_prediction_app.py
@@ -1,1 +1,8 @@
-from salary_prediction import salary_prediction_app
+from flask import Flask
+
+salary_prediction_app = Flask(__name__)
+
+@salary_prediction_app.route('/')
+@salary_prediction_app.route('/index')
+def index():
+    return "<H1>Hello, World, this is Chris!</H1>"
