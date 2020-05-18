025f1cc065ca8dd6e724c798bfe4038014e5d47f
christophercochet <christopher.m.cochet@gmail.com>
2020-05-18 17:34:34 +1200

    first commit

--- salary_prediction/__init__.py
+++ salary_prediction/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import threading
+import socket
+
 
 salary_prediction_app = Flask(__name__)
-flask_thread = threading.Thread(target=salary_prediction_app.run, kwargs={'host':'0.0.0.0','port':80})
+flask_thread = threading.Thread(target=salary_prediction_app.run, kwargs={'host':socket.gethostname(),'port':8000})
 flask_thread.start()
 
 from salary_prediction import routes
