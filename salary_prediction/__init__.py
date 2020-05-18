97ba7bef6b10758507726144be95a920b2996327
christophercochet <christopher.m.cochet@gmail.com>
2020-05-18 16:14:57 +1200

    code ifx multithreading

--- salary_prediction/__init__.py
+++ salary_prediction/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import threading
 
 salary_prediction_app = Flask(__name__)
-flask_thread = threading.Thread(target=app.run, kwargs={'host':'0.0.0.0','port':80})
+flask_thread = threading.Thread(target=salary_prediction_app.run, kwargs={'host':'0.0.0.0','port':80})
 flask_thread.start()
 
 from salary_prediction import routes
