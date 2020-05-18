1529efaf8dc3a56006ab568bc1dfa10c72437e39
christophercochet <christopher.m.cochet@gmail.com>
2020-05-18 17:36:33 +1200

    fix port

--- salary_prediction/__init__.py
+++ salary_prediction/__init__.py
@@ -4,7 +4,7 @@ import socket
 
 
 salary_prediction_app = Flask(__name__)
-flask_thread = threading.Thread(target=salary_prediction_app.run, kwargs={'host':socket.gethostname(),'port':8000})
+flask_thread = threading.Thread(target=salary_prediction_app.run, kwargs={'host':'0.0.0.0','port':8000})
 flask_thread.start()
 
 from salary_prediction import routes
