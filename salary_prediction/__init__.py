a5e48cdc26a15f010f2b67649a5e0f2c962af234
christophercochet <christopher.m.cochet@gmail.com>
2020-05-18 16:02:51 +1200

    adding multi-threading to flask app

--- salary_prediction/__init__.py
+++ salary_prediction/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import threading
 
 salary_prediction_app = Flask(__name__)
+flask_thread = threading.Thread(target=app.run, kwargs={'host':'0.0.0.0','port':80})
+flask_thread.start()
 
 from salary_prediction import routes
--- salary_prediction/routes.py
+++ salary_prediction/routes.py
@@ -1,6 +1,12 @@
 from salary_prediction import salary_prediction_app
+import socket
+import json
+from flask import Flask, jsonify, request
+
+ip_address = socket.gethostbyname(socket.gethostname())
+ip_address
 
 @salary_prediction_app.route('/')
 @salary_prediction_app.route('/index')
 def index():
-    return "<H1>Hello, World!</H1>"
+    return ("<H1>Hello, World!</H1> <H2>API IP Address: {} </H2>".format(ip_address))
