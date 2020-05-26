aa111f009bec850b9c0e54953285549122ec0abb
christophercochet <christopher.m.cochet@gmail.com>
2020-05-26 13:09:51 +1200

    initial commit

--- salary_prediction/routes.py
+++ salary_prediction/routes.py
@@ -1,28 +1,59 @@
-from salary_prediction import salary_prediction_app
+
 import socket
 import json
 import joblib
+import logging
 import requests
 import pandas as pd
 import numpy as np
+
+from salary_prediction import salary_prediction_app
+#from sklearn.ensemble import GradientBoostingRegressor
 from flask import Flask, jsonify, request
+
+logging.basicConfig(level=logging.DEBUG)
+
+def one_hot_encode_feature_df(df, cat_vars=None, num_vars=None, ohe = None):
+    '''performs one-hot encoding on all categorical variables and combines result with continous variables'''
+    cat_df = pd.DataFrame(ohe.transform(df[cat_vars]), columns = ohe.get_feature_names())       
+    num_df = df[num_vars].apply(pd.to_numeric)
+    df = pd.concat([cat_df, num_df], axis=1)
+    return df
+
+#define columns
+salary_col = ['jobId',
+ 'companyId',
+ 'jobType',
+ 'degree',
+ 'major',
+ 'industry',
+ 'yearsExperience',
+ 'milesFromMetropolis']
+
+categorical_vars = ['companyId', 'jobType', 'degree', 'major', 'industry']
+numeric_vars = ['yearsExperience', 'milesFromMetropolis']
 
 ip_address = socket.gethostbyname(socket.gethostname())
 
-trained_model = joblib.load("salary_prediction/model.pkl")
-var_means = joblib.load("salary_prediction/columns_mean.pkl")
+# load model
+salary_prediction_model = joblib.load("salary_prediction/salary_prediction_model.pkl")
+# load ohe
+salary_prediction_ohe = joblib.load("salary_prediction/salary_prediction_ohe.pkl")
+
 
 @salary_prediction_app.route('/')
 @salary_prediction_app.route('/index')
 def index():
-    return ("<H1>Hello, World!</H1> <H2>API IP Address: {} </H2>".format(ip_address))
+    return ("<H1>Salary Prediction End Point!</H1> <H2>API IP Address: {} </H2>".format(ip_address))
 
 @salary_prediction_app.route('/predict', methods=['POST'])
 def predict():
-  data = request.get_json()
-  df_test = pd.DataFrame(data, index=[0])
-  for col, avg_value in var_means.items():
-    df_test[col].fillna(avg_value, inplace=True)
-  prediction = trained_model.predict(df_test)
+  data_unseen = request.get_json(force = True)
+  df_unseen = pd.DataFrame([data_unseen], columns = salary_col)
+  logging.debug(df_unseen)
+  df_unseen = one_hot_encode_feature_df(df_unseen, cat_vars=categorical_vars, num_vars=numeric_vars, ohe = salary_prediction_ohe)
+  logging.debug(df_unseen)
+
+  prediction = salary_prediction_model.predict(df_unseen)
   str_pred = np.array2string(prediction)
   return jsonify(str_pred)
